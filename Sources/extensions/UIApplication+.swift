29a591ad855c8c3b816fc122b850573a0a47f4bb
leesam <toyboy2@hanmail.net>
2024-06-08 23:38:37 +0900

    fix: Cannot convert value of type [String : Any] to expected argument type [UIApplication.OpenExternalURLOptionsKey : Any]

--- Sources/extensions/UIApplication+.swift
+++ Sources/extensions/UIApplication+.swift
@@ -21,7 +21,7 @@ extension UIApplication{
     /**
         os compatible for function to open url
     */
-    private func openCompatible(_ url: URL, options: [String : Any] = [:], completionHandler completion: ((Bool) -> Swift.Void)? = nil){
+    private func openCompatible(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey : Any] = [:], completionHandler completion: ((Bool) -> Swift.Void)? = nil){
         if #available(iOS 10.0, *) {
             self.open(url, options: options, completionHandler: completion)
         } else {
